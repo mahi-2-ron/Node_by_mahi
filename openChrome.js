e7ea12d7b15bced3d3e545e112fd8f875e99bda2
Mahesh <maheshrmadiwalar@gmail.com>
2025-07-25 19:32:34 +0530

    Some changes added

--- openChrome.js
+++ openChrome.js
@@ -2,3 +2,7 @@
 const { exec } = require('child_process');
 exec('code')//  for vs code
 exec('start chrome')// --> for the CHROME
+exec('start chrome');           // opens Chrome
+exec('code .');                 // opens VS Code in current folder
+exec('notepad');                // opens Notepad
+exec('dir');                    // lists current folder contents
